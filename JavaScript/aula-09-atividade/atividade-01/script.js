22f32186b1700a655f74389d859f2ecd5416a487
EDUARDO PROBST <eduardo_probst150@estudante.sesisenai.org.br>
2026-08-06 17:11:09 -0300

    S.E.N.A.I

---
+++ JavaScript/aula-09-atividade/atividade-01/script.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector ("h1");
+
+titulo.addEventListener ("click", function() {
+
+    titulo.textContent = "Manipulação do DOM"
+    
+})
+
+//faltando alguma coisa, talvez colocar o ID pro resultado ou alguma class qque eu deveria ter adicionado????????????????????
---
+++ JavaScript/aula-09/exemplo-03/script.js
@@ -0,0 +1,11 @@
+const titulo = document.querySelector (".titulo");
+const btnAdicionar = document.querySelector (".btnAdicionar");
+const lista = document.querySelector (".lista");
+
+btnAdicionar.addEventListener ("click", function() {
+
+    const li = document.createElement("li")
+    li.textContent = "O farmador de Aura, adicionou mais um item!"
+    lista.appendChild(li)
+    
+})
